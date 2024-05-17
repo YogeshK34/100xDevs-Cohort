52ac5c02fa3e173853c5c894d425121399a38e57
yogeshk34 <khutwadyogesh34@gmail.com>
2024-05-17 22:25:50 +0530

    added some file to Week6 directory

--- Week-6/vite-project/src/App.jsx
+++ Week-6/vite-project/src/App.jsx
@@ -1,17 +1,45 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  // fetch the todos from the backend using useEffect hook
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch("https://sum-server.100xdevs.com/todos")
+        .then(async (res) => {
+          const json = await res.json();
+          console.log("Fetched todos:", json); // Log the response
+          if (Array.isArray(json)) {
+            setTodos(json);
+          } else {
+            console.error("Expected an array but got:", json);
+          }
+        })
+        .catch((err) => console.error("Failed to fetch todos:", err));
+    }, 5000);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div>
-     <Header title = "yogesh1"></Header>
-     <Header title = "yogesh2"></Header>
-     </div>
-  )
+      {todos.length}
+      {todos.map((todo) => (
+        <Todo key={todo.id} title={todo.title} description={todo.description} />
+      ))}
+    </div>
+  );
 }
 
-function Header({title}){
-  return <div>
-    {title}
-  </div>
+function Todo({ title, description }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <h5>{description}</h5>
+    </div>
+  );
 }
 
-export default App
+export default App;

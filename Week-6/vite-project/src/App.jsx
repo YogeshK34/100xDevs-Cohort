e6d839036ecc42deab7ca85820c87ce1ad589134
yogeshk34 <khutwadyogesh34@gmail.com>
2024-05-17 07:51:39 +0530

    created Week 6 directory

---
+++ Week-6/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+
+  return (
+    <div>
+     <Header title = "yogesh1"></Header>
+     <Header title = "yogesh2"></Header>
+     </div>
+  )
+}
+
+function Header({title}){
+  return <div>
+    {title}
+  </div>
+}
+
+export default App

e0dbedd366c8245d8115bcda54ebf6bd0058987e
yogeshk34 <khutwadyogesh34@gmail.com>
2024-05-21 07:35:17 +0530

    created Week-7

---
+++ Week-7/Routing/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import {BrowserRouter,Routes, Route} from 'react-router-dom'
+import './App.css'
+import { Dashboard } from './components/Dashboard'
+import { Landing } from './components/Landing'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route path = "/dashboard" element = {<Dashboard />} />
+          <Route path = "/" element = {<Landing/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App

e68da5e26bdfb42089d7a0a88e1b8ddd46c1cd52
yogeshk34 <khutwadyogesh34@gmail.com>
2024-05-21 19:24:23 +0530

    updates Week-7

--- Week-7/Routing/src/App.jsx
+++ Week-7/Routing/src/App.jsx
@@ -1,35 +1,52 @@
-import React, { lazy, Suspense } from 'react';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import React, { useCallback, useContext, useState } from 'react';
+// import React, { lazy, Suspense } from 'react';
+// import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
+import { CountContext } from './components/Context';
 
-const Landing = lazy(() => import('./components/Landing'));
-const Dashboard = lazy(() => import('./components/Dashboard'));
+// const Landing = lazy(() => import('./components/Landing'));
+// const Dashboard = lazy(() => import('./components/Dashboard'));
 
 function App() {
+  // define state variables
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <BrowserRouter>
-        <Appbar />
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/" element={<Landing />} />
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
+      <CountContext.Provider value={{ count, setCount }}>
+        <Count setCount={setCount} />
+      </CountContext.Provider>
     </div>
   );
 }
 
-function Appbar() {
-  const navigate = useNavigate();
+function Count({ setCount }) {
+  return <div>
+    <Buttons setCount={setCount} />
+    <CounRenderer />
+  </div>
+}
 
-  return (
-    <div>
-      <button onClick={() => navigate("/")}>Landing Page</button>
-      <button onClick={() => navigate("/dashboard")}>Dashboard</button>
-    </div>
-  );
+function CounRenderer({ }) {
+  const count = useContext(CountContext);
+
+  return <div>
+    {count}
+  </div>
+}
+
+function Buttons({ }) {
+  const { count, setCount } = useContext(CountContext);
+
+  return <div>
+    <button onClick={() => {
+      setCount(count + 1);
+    }}>Increase Count</button>
+
+
+    <button onClick={() => {
+      setCount(count - 1);
+    }}>Decrease Count</button>
+  </div>
 }
 
 export default App;
